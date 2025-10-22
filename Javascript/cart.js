ea3f08aca64b14a97ac557b6bd3f516354086e82
Monicah <wakarimangari@gmail.com>
2025-10-22 19:46:40 +0300

    added some funtionality to the cart button

--- Javascript/cart.js
+++ Javascript/cart.js
@@ -1,3 +1,16 @@
 function showQuantity (){
-  
+  console.log(`Items in cart: ${cartCount}`);
+
 }
+function addItem (){
+  
+  cartCount ++;
+  console.log(`Items in cart: ${cartCount}`);
+
+}
+function addItems (){
+  cartCount += 3;
+  console.log(`Items in cart: ${cartCount}`);
+
+}
+let cartCount = 0;
