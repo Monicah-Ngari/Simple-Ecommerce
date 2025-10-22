5ede6cbf98aa1d7390b13053f369ab9a273862a0
Monicah <wakarimangari@gmail.com>
2025-10-22 19:54:09 +0300

    added logic to the buttons

--- Javascript/cart.js
+++ Javascript/cart.js
@@ -2,9 +2,14 @@ function showQuantity (){
   console.log(`Items in cart: ${cartCount}`);
 
 }
+function addCart(){
+    cartCount ++;
+  console.log(`Items in cart : ${cartCount}`);
+  
+}
 function addItem (){
   
-  cartCount ++;
+  cartCount +=2;
   console.log(`Items in cart: ${cartCount}`);
 
 }
@@ -13,4 +18,10 @@ function addItems (){
   console.log(`Items in cart: ${cartCount}`);
 
 }
+function removeItems (){
+  console.log(`Removing items from cart`);
+  console.log(`Items removed from the Cart: ${cartCount}`);
+  console.log(`Items in cart: ${cartCount}`);
+}
+
 let cartCount = 0;
